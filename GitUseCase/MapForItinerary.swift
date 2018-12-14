b974566cc9b938c631f3b527d583d0100f13f1be
Tim <tim@stylah.com>
2018-12-15 08:23:32 +1300

    Add new MapForItinerary feature.

---
+++ GitUseCase/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseCase
+//
+//  Created by Tim Barnaby on 15/12/18.
+//  Copyright © 2018 Tim Barnaby. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
