8c15ca436786de1c7b4d98d9d5bf7aeeb2635397
Tim <tim@stylah.com>
2018-12-15 07:57:31 +1300

    Initial Commit

---
+++ GitUseCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseCase
+//
+//  Created by Tim Barnaby on 15/12/18.
+//  Copyright © 2018 Tim Barnaby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
